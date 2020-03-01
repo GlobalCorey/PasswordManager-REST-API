e0a1b17df622c9176664d3fa45bcb70dc17a35c6
Corey <corey4994@gmail.com>
2020-02-29 22:09:27 -0600

    Restarting work on this project. Added route for getAccounts.

--- routes/accounts.js
+++ routes/accounts.js
@@ -5,11 +5,10 @@ const accountController = require('../controllers/account');
 //Add isAuth check later from middleware
 
 //GET all account data
-router.get('/accounts', );
-
-router.post('/accounts', accountController.addAccount);
+router.get('/accounts', accountController.getAccounts);
 
 //POST new account to account data
+router.post('/accounts', accountController.addAccount);
 
 //DELETE an account with a given id
 
