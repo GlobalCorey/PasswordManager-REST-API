dad4dd0fde856bd751c9b330eb78ee0e5a483b8f
Corey <corey4994@gmail.com>
2021-06-05 16:20:54 -0500

    Additional typescript files added.

--- src/routes/accounts.ts
+++ src/routes/accounts.ts
@@ -1,4 +1,4 @@
-const express = require('express');
+import express = require('express');
 const router = express.Router();
 const accountController = require('../controllers/account');
 const { jwtMiddleware } = require('../middleware/middleware');
--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,4 +1,4 @@
-const express = require('express');
+import express = require('express');
 const router = express.Router();
 const authController = require('../controllers/auth');
 const { jwtMiddleware } = require('../middleware/middleware');
